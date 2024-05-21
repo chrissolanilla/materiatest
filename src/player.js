92c9ecd8de20d1c10c893fd804afdb84ba947fb5
Christopher Solanilla <chrissolanilla@gmail.com>
2024-05-21 16:07:01 -0400

    adds extra required featues

--- src/player.js
+++ src/player.js
@@ -1,1 +1,7 @@
 /* my player js */
+Materia.Engine.start({
+    start: (instance, qset, qsetVersion => {
+        //process the qset and start the app.
+        document.getElementById('root')
+    })
+}) 
